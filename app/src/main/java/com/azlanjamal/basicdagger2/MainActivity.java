9ce52eab3137d2d370bf7312b3b540264e8aa1d0
azlanjamal <akmalazlanjamal@gmail.com>
2018-01-24 12:53:25 +0800

    Add injection A to main activity.

--- app/src/main/java/com/azlanjamal/basicdagger2/MainActivity.java
+++ app/src/main/java/com/azlanjamal/basicdagger2/MainActivity.java
@@ -3,14 +3,31 @@ package com.azlanjamal.basicdagger2;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.azlanjamal.basicdagger2.di.AComponent;
+import com.azlanjamal.basicdagger2.di.AModule;
+import com.azlanjamal.basicdagger2.di.BComponent;
+import com.azlanjamal.basicdagger2.di.BModule;
+import com.azlanjamal.basicdagger2.di.DaggerAComponent;
+import com.azlanjamal.basicdagger2.di.DaggerBComponent;
+
+import javax.inject.Inject;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Inject
+    A a;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        A a = new A();
+        AComponent component = DaggerAComponent
+                .builder()
+                .aModule(new AModule())
+                .build();
+        component.giveTo(this);
+
         a.constructMsg();
     }
 }
