2986375dfa1322529435ea7acae54f4ee8942564
azlanjamal <akmalazlanjamal@gmail.com>
2018-01-24 12:20:49 +0800

    Add class B.

---
+++ app/src/main/java/com/azlanjamal/basicdagger2/B.java
@@ -0,0 +1,12 @@
+package com.azlanjamal.basicdagger2;
+
+/**
+ * Created by akmalazlan on 24/01/2018.
+ */
+
+public class B {
+
+    public B() {
+        System.out.println("Hello from B");
+    }
+}
