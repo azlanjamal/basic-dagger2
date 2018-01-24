ce9d44d548b30ae43826cc4aac4ce7102c919f02
azlanjamal <akmalazlanjamal@gmail.com>
2018-01-24 12:46:35 +0800

    Use inject in A.

--- app/src/main/java/com/azlanjamal/basicdagger2/A.java
+++ app/src/main/java/com/azlanjamal/basicdagger2/A.java
@@ -1,15 +1,29 @@
 package com.azlanjamal.basicdagger2;
 
+import com.azlanjamal.basicdagger2.di.BComponent;
+import com.azlanjamal.basicdagger2.di.BModule;
+import com.azlanjamal.basicdagger2.di.DaggerBComponent;
+
+import javax.inject.Inject;
+
 /**
  * Created by akmalazlan on 24/01/2018.
  */
 
 public class A {
 
+    @Inject
+    B b;
+
     public A() {
+        BComponent component = DaggerBComponent
+                .builder()
+                .bModule(new BModule())
+                .build();
+        component.giveTo(this);
     }
 
-    public void constructMsg(B b) {
+    public void constructMsg() {
         System.out.println(b.showMessage());
     }
 }
