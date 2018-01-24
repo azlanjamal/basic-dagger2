0032a1a700dc49c9c69b9caeba8e258bd1f65465
azlanjamal <akmalazlanjamal@gmail.com>
2018-01-24 12:25:20 +0800

    Instantiate class A.

--- app/src/main/java/com/azlanjamal/basicdagger2/MainActivity.java
+++ app/src/main/java/com/azlanjamal/basicdagger2/MainActivity.java
@@ -9,5 +9,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        A a = new A();
+        a.constructMsg(new B());
     }
 }
