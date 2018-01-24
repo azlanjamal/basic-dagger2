63244ea43188f6068ae75c758006568dcadcc6a6
azlan <azlanjamal@instance.com.my>
2018-01-24 12:10:56 +0800

    Add application class.

---
+++ app/src/main/java/com/azlanjamal/basicdagger2/MyApp.java
@@ -0,0 +1,15 @@
+package com.azlanjamal.basicdagger2;
+
+import android.app.Application;
+
+/**
+ * Created by akmalazlan on 24/01/2018.
+ */
+
+public class MyApp extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
