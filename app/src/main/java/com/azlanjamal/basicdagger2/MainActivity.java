a5188d87c4c3e6684b20781b6ad58eae25901605
azlanjamal <akmalazlanjamal@gmail.com>
2018-01-24 12:46:53 +0800

    Remove argument.

--- app/src/main/java/com/azlanjamal/basicdagger2/MainActivity.java
+++ app/src/main/java/com/azlanjamal/basicdagger2/MainActivity.java
@@ -11,6 +11,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         A a = new A();
-        a.constructMsg(new B());
+        a.constructMsg();
     }
 }
